eacd746f686ef2b235831b06634911b818c77369
rodrigoazlima <rodrigoazlima@hotmail.com>
2026-06-16 18:25:44 -0300

    feat: add attribute and type icon support with dedicated JS files and UI integration

---
+++ data/attribute_images.js
@@ -0,0 +1,11 @@
+// Attribute index matches card.Attribute values (0-6)
+// 0=Dark 1=Earth 2=Fire 3=Light 4=Water 5=Wind 6=Divine
+var attributeImages = [
+  "https://static.wikia.nocookie.net/yugioh/images/d/de/DARK.svg/revision/latest?cb=20120918053848",
+  "https://static.wikia.nocookie.net/yugioh/images/a/a1/EARTH.svg/revision/latest?cb=20120918053843",
+  "https://static.wikia.nocookie.net/yugioh/images/d/d6/FIRE.svg/revision/latest?cb=20120918053839",
+  "https://static.wikia.nocookie.net/yugioh/images/3/39/LIGHT.svg/revision/latest?cb=20120918053832",
+  "https://static.wikia.nocookie.net/yugioh/images/4/40/WATER.svg/revision/latest?cb=20120918052107",
+  "https://static.wikia.nocookie.net/yugioh/images/0/01/WIND.svg/revision/latest?cb=20170920205028",
+  "https://static.wikia.nocookie.net/yugioh/images/7/7c/DIVINE.svg/revision/latest?cb=20120918053906",
+];
---
+++ data/type_images.js
@@ -0,0 +1,31 @@
+// Type index matches cardTypes[] order (0-23)
+// 0=Dragon 1=Spellcaster 2=Zombie 3=Warrior 4=Beast-Warrior 5=Beast
+// 6=Winged Beast 7=Fiend 8=Fairy 9=Insect 10=Dinosaur 11=Reptile
+// 12=Fish 13=Sea Serpent 14=Machine 15=Thunder 16=Aqua 17=Pyro
+// 18=Rock 19=Plant 20=Magic(Spell) 21=Trap 22=Ritual 23=Equip
+var typeImages = [
+  "https://ms.yugipedia.com//c/c7/Dragon.png",
+  "https://ms.yugipedia.com//2/2f/Spellcaster.png",
+  "https://ms.yugipedia.com//c/c3/Zombie.png",
+  "https://ms.yugipedia.com//9/97/Warrior.png",
+  "https://ms.yugipedia.com//1/1d/Beast-Warrior.png",
+  "https://ms.yugipedia.com//1/17/Beast.png",
+  "https://ms.yugipedia.com//0/01/Winged_Beast.png",
+  "https://ms.yugipedia.com//6/6e/Fiend.png",
+  "https://ms.yugipedia.com//4/43/Fairy.png",
+  "https://ms.yugipedia.com//7/79/Insect.png",
+  "https://ms.yugipedia.com//8/89/Dinosaur.png",
+  "https://ms.yugipedia.com//9/90/Reptile.png",
+  "https://ms.yugipedia.com//f/fb/Fish.png",
+  "https://ms.yugipedia.com//2/2f/Sea_Serpent.png",
+  "https://ms.yugipedia.com//1/18/Machine.png",
+  "https://ms.yugipedia.com//3/3d/Thunder.png",
+  "https://ms.yugipedia.com//4/44/Aqua.png",
+  "https://ms.yugipedia.com//c/c8/Pyro.png",
+  "https://ms.yugipedia.com//0/0b/Rock.png",
+  "https://ms.yugipedia.com//d/d1/Plant.png",
+  "https://ms.yugipedia.com//d/db/Spell.svg",
+  "https://ms.yugipedia.com//5/5e/Trap.svg",
+  "https://ms.yugipedia.com//7/70/Ritual.png",
+  "https://ms.yugipedia.com//9/90/Equip.svg",
+];
